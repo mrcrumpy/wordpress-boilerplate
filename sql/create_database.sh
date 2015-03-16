c1ddba6f45efc6cefeff8fe62f85ee675d56114c
Daniel Crump <d.crump@crolla-lowis.de>
2015-03-16 16:27:40 +0100

    Added SQL-Dump + Importscript

--- sql/create_database.sh
+++ sql/create_database.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 DATABASE="wp_wordpress"
-# SQLDUMP="testdaten.sql"
+SQLDUMP="wp_wordpress.sql"
 
-# if [ $1 ]
-# then
-#   OPTFILE=${1##*/}
+if [ $1 ]
+then
+  OPTFILE=${1##*/}
 
-#   if [ -f sql/$OPTFILE ]
-#   then
-#     SQLDUMP=$OPTFILE
-#   else
-#     echo "Could not find the SQL file $OPTFILE"
-#   fi
-# fi
+  if [ -f sql/$OPTFILE ]
+  then
+    SQLDUMP=$OPTFILE
+  else
+    echo "Could not find the SQL file $OPTFILE"
+  fi
+fi
 
 CONTAINER=`docker-compose ps | grep _db_ | cut -d' ' -f1`
 if [ $CONTAINER ]
@@ -21,8 +21,8 @@ then
   echo "Creating/Cleaning database $DATABASE in docker container $CONTAINER"
   docker exec -it $CONTAINER mysql -h127.0.0.1 -u"admin" -p"123" -e"DROP DATABASE IF EXISTS $DATABASE"
   docker exec -it $CONTAINER mysql -h127.0.0.1 -u"admin" -p"123" -e"CREATE DATABASE IF NOT EXISTS $DATABASE"
-  # echo "Importing MySQL dump from sql/$SQLDUMP"
-  # docker exec -it $CONTAINER mysql -h127.0.0.1 -u"admin" -p"123" -e"source /root/sql-import/$SQLDUMP" $DATABASE
+  echo "Importing MySQL dump from sql/$SQLDUMP"
+  docker exec -it $CONTAINER mysql -h127.0.0.1 -u"admin" -p"123" -e"source /root/sql-import/$SQLDUMP" $DATABASE
 else
   echo -e "Could not find a database container.\nDid you start it?\n"
   echo "Running docker-compose ps for you..."
