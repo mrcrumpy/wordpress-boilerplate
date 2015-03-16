9808cff6256bb3715a61e713daa2690b1f125220
Daniel Crump <d.crump@crolla-lowis.de>
2015-03-16 14:07:39 +0100

    underscore als standarttemplate + gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var SRC_PATH = './src';
-var DIST_PATH = './wordpress/wp-content/templates/';
+var SRC_PATH = './src/underscores';
+var DIST_PATH = './public/content/themes/underscores';
 
 var TMP_PATH = DIST_PATH;
 
 var gulp = require('gulp');
 var g = require('gulp-load-plugins')();
+
+var rename = require('gulp-rename');
 
 var runSequence = require('run-sequence');
 var memRev = require('./utils/gulp-memrev');
@@ -24,14 +26,13 @@ gulp.task('clean', function (done) {
 
 gulp.task('views', function () {
   var stream = gulp.src([
-    SRC_PATH + '/views/**/*.twig',
-    SRC_PATH + '/index.php',
-    SRC_PATH + '/.htaccess'],
+    SRC_PATH + '/**/*.php',
+    SRC_PATH + '/screenshot.png',
+    SRC_PATH + '/js/*.js',
+    '!' + SRC_PATH + '/js/index.js'
+    ],
     { base: SRC_PATH });
 
-  if (DIST) {
-    stream = stream.pipe(memRev.replace());
-  }
 
   stream = stream.pipe(gulp.dest(CURRENT_PATH));
 
@@ -58,24 +59,8 @@ gulp.task('assets', function () {
     .pipe(gulp.dest(CURRENT_PATH));
 });
 
-
-gulp.task('watch', function () {
-  gulp.watch([
-    SRC_PATH + '/**/*.php',
-    SRC_PATH + '/**/*.twig' ], ['views']);
-
-  // gulp.watch(SRC_PATH + '/**/*.html', ['html']);
-  gulp.watch(SRC_PATH + '/**/*.scss', ['sass']);
-  gulp.watch(SRC_PATH + '/**/*.js',   ['browserify']);
-});
-
-gulp.task('serve', function (done) {
-  g.livereload.listen();
-  runSequence('clean', 'lint', ['browserify', 'sass', 'assets', 'images'], 'views', 'watch', done);
-});
-
 gulp.task('sass', function () {
-  var stream = gulp.src(SRC_PATH + '/sass/main.scss')
+  var stream = gulp.src(SRC_PATH + '/sass/style.scss')
     .pipe(g.plumber({errorHandler: g.notify.onError('Sass: <%= error.message %>')}))
     .pipe(g.sourcemaps.init())
     .pipe(g.sass({
@@ -92,8 +77,6 @@ gulp.task('sass', function () {
 
   if (DIST) {
     stream = stream
-      .pipe(g.rev())
-      .pipe(memRev())
       .pipe(g.minifyCss());
   } else {
     stream = stream
@@ -101,11 +84,11 @@ gulp.task('sass', function () {
       .pipe(g.livereload());
   }
 
-  return stream.pipe(gulp.dest(CURRENT_PATH + '/css/'));
+  return stream.pipe(gulp.dest(DIST_PATH));
 });
 
 gulp.task('browserify', ['lint'], function() {
-  var stream = gulp.src(SRC_PATH + '/js/index.js')
+  var stream = gulp.src(SRC_PATH + '/**/js/index.js')
     .pipe(g.plumber({errorHandler: g.notify.onError('Browserify: <%= error.message %>')}))
     .pipe(g.browserify2({
       fileName: 'bundle.js',
@@ -117,19 +100,33 @@ gulp.task('browserify', ['lint'], function() {
 
   if (DIST) {
     stream = stream
-      .pipe(g.rev())
-      .pipe(memRev())
       .pipe(g.uglify());
   }
 
-  stream = stream.pipe(gulp.dest(CURRENT_PATH + '/js/'));
+  stream = stream.pipe(gulp.dest(CURRENT_PATH + '/js'));
 
   if (!DIST) stream = stream.pipe(g.livereload());
   return stream;
 });
 
+
+gulp.task('watchfiles', function () {
+  gulp.watch([
+    SRC_PATH + '/**/*.php'
+     ], ['views']);
+
+  // gulp.watch(SRC_PATH + '/**/*.html', ['html']);
+  gulp.watch(SRC_PATH + '/**/*.scss', ['sass']);
+  gulp.watch(SRC_PATH + '/**/*.js',   ['browserify']);
+});
+
+gulp.task('watch', function (done) {
+  g.livereload.listen();
+  runSequence('clean', 'lint', [ 'browserify' ,'sass', 'assets', 'images'], 'views', 'watchfiles', done);
+});
+
 gulp.task('lint', function () {
-  return gulp.src(SRC_PATH + '/js/**/*.js')
+  return gulp.src(SRC_PATH + '/**/js/**/*.js')
     .pipe(g.plumber({errorHandler: g.notify.onError('<%= error.message %>')}))
     .pipe(g.cached('linting'))
     .pipe(g.jshint())
@@ -147,7 +144,7 @@ gulp.task('test', function (done) {
 gulp.task('dist', function (done) {
   DIST = true;
   CURRENT_PATH = DIST_PATH;
-  runSequence('clean', 'lint', ['browserify', 'sass', 'images', 'assets'], 'views', done);
+  runSequence('clean', 'lint', [ 'browserify', 'sass', 'images', 'assets'], 'views', done); 
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
